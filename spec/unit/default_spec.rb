8fc88fe4929d633e9073e6f48f9f5d88b8133501
Arjen de Rijke <arjenderijke@gmail.com>
2016-03-29 15:31:56 +0200

    the jenkins cookbook provides the custom matchers needed to test all resources

--- spec/unit/default_spec.rb
+++ spec/unit/default_spec.rb
@@ -4,7 +4,12 @@ describe 'jenkins_dev::default' do
   let(:chef_run) {
     ChefSpec::SoloRunner.new.converge(described_recipe)
   }
+
   it 'include recipe jenkins master' do
     expect(chef_run).to include_recipe('jenkins::master')
+  end
+
+  it 'install jenkins_plugin greenballs' do
+    expect(chef_run).to install_jenkins_plugin('greenballs')
   end
 end
