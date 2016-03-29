efd0587a84d43861eacc185e40c52c9b498e705a
Arjen de Rijke <arjenderijke@gmail.com>
2016-03-29 11:03:05 +0200

    install jenkins master in default cookbook

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,9 @@
 name             'jenkins_dev'
-maintainer       'YOUR_COMPANY_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Arjen de Rijke'
+maintainer_email 'arjenderijke@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures jenkins_dev'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends 'jenkins', '~> 2.4.1'
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,11 @@
 # Cookbook Name:: jenkins_dev
 # Recipe:: default
 #
-# Copyright 2016, YOUR_COMPANY_NAME
+# Copyright 2016, Arjen de Rijke
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe 'jenkins::master'
+
+jenkins_plugin 'greenballs'
